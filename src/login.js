1f50174625e99cce4f5c5999c56cb3fc44aea48d
Yang-ah <yangah0409@gmail.com>
2022-11-08 22:05:28 +0900

    added greeting in login.js

--- src/login.js
+++ src/login.js
@@ -1,13 +1,32 @@
 const loginForm = document.querySelector(".login-form");
 const name = document.querySelector(".name");
 const loginBtn = document.querySelector(".login-btn");
+const greetDiv = document.querySelector(".greet");
 
 const LOGIN_KEY = "username";
+const HIDDEN_CLASSNAME = "hidden";
+const savedUsername = localStorage.getItem(LOGIN_KEY);
 
 function login(e) {
   let username = name.value;
   e.preventDefault();
   localStorage.setItem(LOGIN_KEY, username);
+  greeting(username);
+}
+
+function greeting(username) {
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greetDiv.classList.remove(HIDDEN_CLASSNAME);
+  greetDiv.textContent = `Have a good day, ${username}`;
+}
+
+// main
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  greetDiv.classList.add(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", login);
+} else {
+  greeting(savedUsername);
 }
 
 loginForm.addEventListener("submit", login);
