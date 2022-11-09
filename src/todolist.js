9c55dfeb23782e4d51b3b330f26589c2cf118e21
Yang-ah <yangah0409@gmail.com>
2022-11-09 16:37:57 +0900

    modified todolist.file

--- src/todolist.js
+++ src/todolist.js
@@ -1,49 +1,55 @@
 const todoForm = document.querySelector(".todolist-form");
-const todoInput = document.querySelector(".todo-innput");
+const todoInput = document.querySelector(".todo-input");
 const todoUl = document.querySelector(".todolist-ul");
-const TODO_KEY = "todo";
 const todoBtn = document.querySelector(".todolist-btn");
+const addBtn = document.querySelector(".todolist-add-btn");
+
+const TODO_KEY = "todo";
 
 let todoList = [];
-
-const saveTodo = (todo) => localStorage.setItem(TODO_KEY, JSON.stringify(todo));
-const displayTodo = (newTodoObj) => {
-  const li = document.createElement("li");
-  li.id = newTodoObj.id;
-  const div = document.createElement("div");
-  div.innerText = newTodoObj.text;
-  const button = document.createElement("button");
-  button.innerText = "❌";
-
-  todoUl.appendChild(li);
-  li.appendChild(div);
-  li.appendChild(button);
-};
-
-const handleSubmit = (e) => {
-  const newTodo = todoInput.value;
-  const newTodoObj = {
-    text: newTodo,
-    id: Date.now(),
-  };
-  e.preventDefault();
-  saveTodo();
-  todoList.push(newTodo);
-  todoInput.value = "";
-  displayTodo(newTodoObj);
-};
-
-if (localStorage.getItem("username") === null) {
-  todoForm.classList.add("hidden");
-} else {
-  todoForm.classList.remove("hidden");
-  todoForm.classList.add("flex");
-  todoForm.addEventListener("submit", handleSubmit);
-}
-
-todoBtn.addEventListener("click", activeUl);
 
 function activeUl(e) {
   e.preventDefault();
   todoUl.classList.toggle("hidden");
 }
+
+const saveToDos = () =>
+  localStorage.setItem(TODO_KEY, JSON.stringify(todoList));
+
+const displayTodo = (todoOBJ) => {
+  const li = document.createElement("li");
+  li.id = todoOBJ.id;
+
+  const text = document.createElement("span");
+  text.textContent = todoOBJ.text;
+
+  const Xbtn = document.createElement("span");
+  Xbtn.textContent = "❌";
+
+  const chkBox = document.createElement("i");
+  chkBox.className = "fa-regular fa-square";
+
+  todoUl.appendChild(li);
+  li.appendChild(chkBox);
+  li.appendChild(text);
+  li.appendChild(Xbtn);
+};
+
+function sendTodo(e) {
+  e.preventDefault();
+  const todo = todoInput.value;
+  const todoOBJ = {
+    text: todo,
+    id: Date.now(),
+  };
+  todoList.push(todoOBJ);
+  saveToDos();
+  displayTodo(todoOBJ);
+  todoInput.value = "";
+}
+
+/* event listener */
+
+todoBtn.addEventListener("click", activeUl);
+
+addBtn.addEventListener("click", sendTodo);
